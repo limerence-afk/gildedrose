f3319ce8f63284849dcf167c18de9f91108995ce
Bogdan <maxim.bogdan98@gmail.com>
2022-09-28 10:35:37 +0300

    qqq

---
+++ QualityRangeChecker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp
+{
+    public static class QualityRangeChecker
+    {
+        public static int CheckQualityAndSetInRange(this Item item, int maxQuality, int minQuality)
+        {
+            return Math.Min(maxQuality, Math.Max(0, item.Quality));
+        }  
+    }
+}
